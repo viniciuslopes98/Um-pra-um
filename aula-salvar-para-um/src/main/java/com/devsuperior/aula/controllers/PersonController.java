bf26e324012f90a78b69aa5ad0b0d90585f8db27
Vinicius Lopes <vinicius6.lopes6@gmail.com>
2023-06-13 01:38:33 -0300

    Save PersonDTO

--- aula-salvar-para-um/src/main/java/com/devsuperior/aula/controllers/PersonController.java
+++ aula-salvar-para-um/src/main/java/com/devsuperior/aula/controllers/PersonController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devsuperior.aula.dto.PersonDTO;
 import com.devsuperior.aula.dto.PersonDepartmentDTO;
 import com.devsuperior.aula.services.PersonService;
 
@@ -21,8 +22,16 @@ public class PersonController {
 	private PersonService service;
 	
 	
+//	@PostMapping
+//	public ResponseEntity<PersonDepartmentDTO> insert(@RequestBody PersonDepartmentDTO dto) {
+//		dto = service.insert(dto);
+//		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+//				.buildAndExpand(dto.getId()).toUri();
+//		return ResponseEntity.created(uri).body(dto);
+//	}
+	
 	@PostMapping
-	public ResponseEntity<PersonDepartmentDTO> insert(@RequestBody PersonDepartmentDTO dto) {
+	public ResponseEntity<PersonDTO> insert(@RequestBody PersonDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
--- aula-salvar-para-um/src/main/java/com/devsuperior/aula/services/PersonService.java
+++ aula-salvar-para-um/src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -2,6 +2,8 @@ package com.devsuperior.aula.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.devsuperior.aula.dto.PersonDTO;
 import com.devsuperior.aula.dto.PersonDepartmentDTO;
 import com.devsuperior.aula.entities.Department;
 import com.devsuperior.aula.entities.Person;
@@ -18,19 +20,26 @@ public class PersonService {
 	private DepartmentRepository departmentRepository;
 	
 	public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {
-		
 		Person entity = new Person();
 		entity.setName(dto.getName());
 		entity.setSalary(dto.getSalary());
-		
 		Department dept = departmentRepository.getReferenceById(dto.getDepartment().getId());
-		
 		//Department dept = new Department();
 		//dept.setId(dto.getDepartment().getId());
-		
 		entity.setDepartment(dept);
 		entity = repository.save(entity);
-		
 		return new PersonDepartmentDTO(entity);
+	}
+	
+	public PersonDTO insert(PersonDTO dto) {
+		Person entity = new Person();
+		entity.setName(dto.getName());
+		entity.setSalary(dto.getSalary());
+		Department dept = departmentRepository.getReferenceById(dto.getDepartmentId());
+		//Department dept = new Department();
+		//dept.setId(dto.getDepartment().getId());
+		entity.setDepartment(dept);
+		entity = repository.save(entity);
+		return new PersonDTO(entity);
 	}
 }
